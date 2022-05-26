40403fa81b7fc66c0820c6fed4431f33b7c3743a
Anastasiya Klyshko <new-emerald@yandex.ru>
2022-05-26 22:53:09 +1000

    add change

--- index.js
+++ index.js
@@ -23,12 +23,11 @@ function check() {
 let result1 = document.getElementById("name").value;
 let result2 = document.getElementById("lastname").value;
 let result3 = document.getElementById("birthday").value;
-//let result4 = document.getElementById("female").value;
-//let result5 = document.getElementById("male").value;
+let result4 = document.getElementById("female").checked;
+let result5 = document.getElementById("male").checked;
 let result6 = document.getElementById("box").value;
 let result7 = document.getElementById("code").value;
 let result8 = document.getElementById("phone").value;
-
 
 if (result1 =='') {
     alert ("Нужно заполнить Имя");
@@ -36,12 +35,14 @@ if (result1 =='') {
     alert ("Нужно заполнить Фамилию");
 } else if (result3=='') {
     alert ("Нужно заполнить Дату Рождения");
-}  else if (result6=='') {
+} else if (result4==false&&result5==false)  {
+    alert ("Нужно выбрать пол");
+} else if (result6=='') {
     alert ("Нужно придумать Имя ящика");
 }  else if (result7 =='') {
     alert ("Нужно придумать пароль");
 }  else if (result8 =='') {
-    alert("Нужно заполнить номер телефона")
+    alert ("Нужно заполнить номер телефона");
 } else {
 alert ("Добро пожаловать, " + result1 +" "+ result2 + "!");
 }
